dcba5c1123ee1a912dc6f3ac7143654d1e9941d6
Said Bennana <115021608+SaidBennana@users.noreply.github.com>
2024-07-29 16:07:56 +0100

    backend_update home setting

--- controllers/Store Customizations/HomeSetting.js
+++ controllers/Store Customizations/HomeSetting.js
@@ -1,9 +1,10 @@
+const { default: mongoose } = require("mongoose");
 const Setting_md = require("../../models/Store Customizations/HomeSetting");
 
 const get = async (_, res) => {
   try {
     const setting = await Setting_md.findOne();
-    if (!setting) return res.status(404).json({ message: "Setting not found" });
+    if (!setting) return res.json({ message: "Setting not found" });
     return res.json(setting);
   } catch (error) {
     return res.status(500).json({ message: error.message });
@@ -13,7 +14,7 @@ const update = async (req, res) => {
   try {
     const one = await Setting_md.findOne();
     const setting = await Setting_md.findOneAndUpdate(
-      { _id: one?._id || undefined },
+      { _id: one?._id ?? new mongoose.Types.ObjectId() },
       req.body,
       {
         upsert: true,
