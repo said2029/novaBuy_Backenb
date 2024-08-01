541c359cfbb82d049403ffe35360a7a27e0c97e6
Said Bennana <115021608+SaidBennana@users.noreply.github.com>
2024-08-01 14:00:25 +0100

    Update schema Attribute

--- models/Attributes_model.js
+++ models/Attributes_model.js
@@ -5,6 +5,7 @@ const attirbutesShcema = mongoose.Schema(
     name: { type: String, required: true },
     option: { type: String, defualt: "dropdwon", required: true },
     values: [{ name: String, publiched: Boolean }],
+    published: { type: Boolean, defualt: true },
   },
   { timestamps: true }
 );
